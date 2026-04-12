cbabbb72cd728b72114b2e70769f5254db87e451
ahmediug24cs-maker <ahmedi.ug.24.cs@francisxavier.ac.in>
2026-04-12 14:00:29 +0530

    Fix root grader exports for validator compatibility

--- graders.py
+++ graders.py
@@ -1,20 +1,3 @@
-from .models import EnvState
-from .tasks import get_task_easy, get_task_hard, get_task_medium
-
-
-def grade_easy(final_state: EnvState) -> float:
-    """Grade final state for easy-memory-leak task."""
-    return get_task_easy()[2].grade(final_state)
-
-
-def grade_medium(final_state: EnvState) -> float:
-    """Grade final state for medium-traffic-spike task."""
-    return get_task_medium()[2].grade(final_state)
-
-
-def grade_hard(final_state: EnvState) -> float:
-    """Grade final state for hard-cascading-failure task."""
-    return get_task_hard()[2].grade(final_state)
-
+from src.tasks import grade_easy, grade_hard, grade_medium
 
 __all__ = ["grade_easy", "grade_medium", "grade_hard"]
